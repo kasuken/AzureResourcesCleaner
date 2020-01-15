aa803fa1f890d8ba00601c9027436dd99372d89d
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2020-01-15 12:27:30 +0100

    0.0.1v

---
+++ src/AzResCleanerTimeTrigger/run.ps1
@@ -0,0 +1,30 @@
+param($Timer)
+
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+$expiredResources = Search-AzGraph -Query 'where todatetime(tags.expireOn) < now() | project id, name'
+
+foreach ($r in $expiredResources) {
+    Write-Host "==> Deleting  $($r.name)...";
+    Remove-AzResource -ResourceId $r.id -Force
+}
+
+$expiredResources = Get-AzResourceGroup;
+
+foreach ($resourceGroup in $expiredResources) {
+    $count = (Get-AzResource | Where-Object { $_.ResourceGroupName -match $resourceGroup.ResourceGroupName }).Count;
+    if ($count -eq 0) {
+        Write-Host "==> $($resourceGroup.ResourceGroupName) is empty. Deleting it...";
+        Remove-AzResourceGroup -Name $resourceGroup.ResourceGroupName -Force
+    }
+}
+
+$expiredResources = Get-AzResourceGroup | Where-Object { $_.Tags.expireOn -and [DateTime] $_.Tags.expireOn -lt $currentUTCtime }
+
+Foreach ($resourceGroup in $expiredResources) {
+    Write-Host "==> $($resourceGroup.ResourceGroupName) is expired. Deleting it..."
+    Remove-AzResourceGroup -Name $resourceGroup.ResourceGroupName -Force
+}
+
+# Write an information log with the current time.
+Write-Host "Azure Resources Cleaner ran at : $currentUTCtime"
